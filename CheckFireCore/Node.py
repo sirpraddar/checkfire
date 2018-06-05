7dd97579c8987c5faa019f527d4c3c0c41804047
sirpraddar <sir.praddar@gmail.com>
2018-06-05 10:35:52 +0200

    Node.py skeleton

---
+++ CheckFireCore/Node.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+    def execPackage (self,package):
+        pass
+
+    def loadPackage (self,package):
+        pass
+
+    def getResults(self):
+        pass
