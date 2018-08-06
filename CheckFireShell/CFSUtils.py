33e0feb8021faf25a267e5aa588611b4a98ef159
sirpraddar <sir.praddar@gmail.com>
2018-08-06 19:36:45 +0200

    Added function to check names

--- CheckFireShell/CFSUtils.py
+++ CheckFireShell/CFSUtils.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+import re
+
 
 def checkPathExists(path):
     return Path(path).is_file()
+
+
+def checkPureName(name):
+    pattern = re.compile("^[A-Za-z0-9]+$")
+    return pattern.match(name)
