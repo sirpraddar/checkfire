06288350fc1056cb6d565993ebcd5d0cb6a7e433
sirpraddar <sir.praddar@gmail.com>
2018-10-09 13:00:28 +0200

    fixed timeout to ping

--- plainScripts/ping.sh
+++ plainScripts/ping.sh
@@ -3,4 +3,16 @@
 if [ -z ${COUNT+x} ]; then COUNT=4; fi
 if [ -z ${INTERVAL+x} ]; then INTERVAL=0.2; fi
 
-ping -c $COUNT -i $INTERVAL $DESTIP
+#ping -c $COUNT -i $INTERVAL $DESTIP
+
+CODE=0
+
+for i in $(seq 1 $COUNT); do
+    timeout --preserve-status --signal INT $INTERVAL ping -c 1 $DESTIP > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "ping $i failed"
+        CODE=$(expr $CODE + 1)
+    fi
+done
+echo "Packets lost: $CODE"
+exit $CODE
